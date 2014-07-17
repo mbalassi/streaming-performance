e5c1bf9c4c35c0ba7c7a973d19d62ea63aade95c
Tofi007 <bartam82@gmail.com>
2014-07-17 12:37:39 +0200

    path fix

--- src/test/resources/Performance/storm-run-test.sh
+++ src/test/resources/Performance/storm-run-test.sh
@@ -14,7 +14,7 @@ if [ -d "${toDir}" ] ; then
 	rm -r $toDir/$testParams/*;
 	mkdir $toDir/$testParams;
 
-	ssh -n storm@dell150.ilab.sztaki.hu "timeout ${length} ./${stormDir}/bin/storm jar ./$stormDir/lib/${jarFile} storm.performance.WordCountTopology cluster /home/storm/${stormDir}/resources/hamlet.txt /home/storm/${stormDir}/log/counter/ ${paramsWithSpace}"
+	ssh -n storm@dell150.ilab.sztaki.hu "timeout ${length} ./${stormDir}/bin/storm jar ./$stormDir/lib/${jarFile} storm.performance.WordCountTopology cluster /home/storm/${stormDir}/resources/hamlet.txt /home/storm/${stormDir}/logs/counter/ ${paramsWithSpace}"
     #ssh -n storm@dell150.ilab.sztaki.hu "$stormDir/bin/stop-cluster.sh; sleep 2; $stormDir/bin/start-cluster.sh"
 
 	echo "job finished"

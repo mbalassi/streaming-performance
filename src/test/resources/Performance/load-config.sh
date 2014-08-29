0b27240d50b0791d06d0ca7608d5c3ce30915d8e
Tofi007 <bartam82@gmail.com>
2014-08-29 14:32:15 +0200

    pelda futtatasok javitas es flink host configban

--- src/test/resources/Performance/load-config.sh
+++ src/test/resources/Performance/load-config.sh
@@ -16,19 +16,21 @@ if [ $# = 1 ]; then
         sed -n '5{p;q}' ${thisDir}/config
     elif [ $arg = "strato-slaves" ]; then
         sed -n '6{p;q}' ${thisDir}/config
-    elif [ $arg = "strato-master-rcp-port" ]; then
+    elif [ $arg = "strato-host" ]; then
         sed -n '7{p;q}' ${thisDir}/config
-    elif [ $arg = "hadoop-user" ]; then
+    elif [ $arg = "strato-master-rcp-port" ]; then
         sed -n '8{p;q}' ${thisDir}/config
-    elif [ $arg = "hadoop-master" ]; then
+    elif [ $arg = "hadoop-user" ]; then
         sed -n '9{p;q}' ${thisDir}/config
-    elif [ $arg = "hadoop-hdfssavedir" ]; then
+    elif [ $arg = "hadoop-master" ]; then
         sed -n '10{p;q}' ${thisDir}/config
-    elif [ $arg = "spark-user" ]; then
+    elif [ $arg = "hadoop-hdfssavedir" ]; then
         sed -n '11{p;q}' ${thisDir}/config
-    elif [ $arg = "spark-master" ]; then
+    elif [ $arg = "spark-user" ]; then
         sed -n '12{p;q}' ${thisDir}/config
-    elif [ $arg = "spark-slaves" ]; then
+    elif [ $arg = "spark-master" ]; then
         sed -n '13{p;q}' ${thisDir}/config
+    elif [ $arg = "spark-slaves" ]; then
+        sed -n '14{p;q}' ${thisDir}/config
     fi
 fi
--- src/test/resources/Performance/load-flink-config.sh
+++ src/test/resources/Performance/load-flink-config.sh
@@ -3,4 +3,5 @@
 stratoUser=$(./load-config.sh strato-user)
 stratoMaster=$(./load-config.sh strato-master)
 stratoSlaves=$(./load-config.sh strato-slaves)
+stratoHost=$(./load-config.sh strato-host)
 stratoRcpPort=$(./load-config.sh strato-master-rcp-port)
